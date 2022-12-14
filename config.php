5a774c8568355f5b572435f225989958144de3c1
Ngô Hoàng Phúc <107487039+PhucPhuc1304@users.noreply.github.com>
2022-12-14 16:17:34 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
         "Database" => "cnpm",
     );
     $cookie_name = 'siteAuth';
-    $cookie_time = (60); // 30 days
+    $cookie_time = (3600 * 24 * 30); // 30 days
     $conn = sqlsrv_connect('cnpmhutech.database.windows.net', $conn_array);
     if( $conn === false ) {
         die( print_r( sqlsrv_errors(), true));
